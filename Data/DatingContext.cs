fea56a1cb086e3a5f5c19ac37e747dd4d1a7edcc
Indika <you@example.com>
2020-04-15 12:50:30 +1200

    Initial Commit

---
+++ Data/DatingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dating.Models;
+
+namespace Dating.Data
+{
+    public class DatingContext : DbContext
+    {
+        public DatingContext (DbContextOptions<DatingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dating.Models.Value> Value { get; set; }
+    }
+}
